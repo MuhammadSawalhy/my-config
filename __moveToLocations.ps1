191b6d44c4d77e30ca2b1b10595ad373812ae3be
Mohammed Samir <ms.2052001@gmail.com>
2020-09-01 18:11:17 +0200

    Add .gitignore_global file

--- __moveToLocations.ps1
+++ __moveToLocations.ps1
@@ -2,6 +2,10 @@ function cptodir($path, $dir){
 	cp $path "$dir\$(Split-path $path -leaf)"
 }
 
+# copy to directory replacing or create the file there
 cptodir ./.gitconfig $HOME
+cptodir ./.gitignore_global $HOME
+
+# copy to file path, replacing it
 cp 	./Microsoft.PowerShell_profile.ps1 $PROFILE
 

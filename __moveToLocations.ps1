545f0e379d4ebd568b733615f4952c78d673c137
Mohammed Samir <ms.2052001@gmail.com>
2020-09-01 23:55:19 +0200

    Add custom path PS functions, use for loop to copy config files.
    Add PS function path-resolve and path-relative to help me use the foor loop to iterate upon $config PS array contianing [pscustomobject] with File name and Dest properties.

--- __moveToLocations.ps1
+++ __moveToLocations.ps1
@@ -1,11 +1,43 @@
 function cptodir($path, $dir){
-	cp $path "$dir\$(Split-path $path -leaf)"
+    cp $path "$dir\$(Split-Path $path -leaf)"
 }
 
-# copy to directory replacing or create the file there
-cptodir ./.gitconfig $HOME
-cptodir ./.gitignore_global $HOME
+function get-filedir($path, $dir){
+    echo (Split-Path $path)
+}
 
-# copy to file path, replacing it
-cp 	./Microsoft.PowerShell_profile.ps1 $PROFILE
+function get-filename($path, $dir){
+    echo (Split-Path $path -Leaf)
+}
 
+function path-resolve($Dir, $Path){
+	$resolved = [System.IO.Path]::GetFullPath((Join-Path $Dir $Path))
+	echo $resolved
+};
+
+function path-relative($Path){
+	path-resolve $PWD.path $Path
+};
+
+$configs = @(
+	[pscustomobject]@{FileName='.gitconfig'; Dest=$HOME}
+	[pscustomobject]@{FileName='.gitignore_global'; Dest=$HOME}
+	[pscustomobject]@{FileName='.npmrc'; Dest=$HOME}
+	[pscustomobject]@{FileName='Microsoft.PowerShell_profile.ps1'; Dest=(Split-Path $PROFILE)}
+)
+
+For ($i=0; $i -lt $configs.Length; $i++) {
+	if(split-path $configs[$i].Dest -IsAbsolute){
+		$file = path-relative $configs[$i].FileName
+		$dest = $configs[$i].Dest
+		cp  $file $dest
+		echo "Copy `"$file`"`nTo `"$dest`""
+		echo "------------------------"
+	} else {
+		$file = path-relative $configs[$i].FileName
+		$dest = path-relative $configs[$i].Dest
+		cp  $file $dest
+		echo "Copy `"$file`"`nTo `"$dest`""
+		echo "------------------------"
+	}
+}

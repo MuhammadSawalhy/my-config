ce555749a6a8ce2b4495f5d4b3cb0b412d474bdd
Mohammed Samir <ms.2052001@gmail.com>
2020-09-10 12:03:22 +0200

    Check if the config file is not changed,,, more elegent updating

--- __moveToLocations.ps1
+++ __moveToLocations.ps1
@@ -1,49 +1,92 @@
-param ([string]$f)
 
-
-function cptodir($path, $dir){
-    cp $path "$dir\$(Split-Path $path -leaf)"
+function file-dir($path){
+    if (!(Test-Path $path)) {
+        throw ("$path doesn't exist")
+    }
+    if (!(Test-Path -Path $path -PathType Leaf)){
+        throw "can't get file dir of $path"
+    }
+    Write-Output (Split-Path $path)
 }
 
-function get-filedir($path, $dir){
-    echo (Split-Path $path)
-}
-
-function get-filename($path, $dir){
-    echo (Split-Path $path -Leaf)
+function file-name($path){
+    if (!(Test-Path $path)) {
+        throw ("$path doesn't exist")
+    }
+    if (!(Test-Path $path -PathType Leaf)){
+        throw ("can't get file name of $path")
+    }
+    Write-Output (Split-Path $path -Leaf)
 }
 
 function path-resolve($Dir, $Path){
-	$resolved = [System.IO.Path]::GetFullPath((Join-Path $Dir $Path))
-	echo $resolved
-};
+    if(Split-Path $Path -IsAbsolute){
+        Write-Output $Path; return
+    }
+	$resolved = [IO.Path]::GetFullPath((Join-Path $Dir $Path))
+	Write-Output $resolved
+}
 
 function path-relative($Path){
-	path-resolve $PSScriptRoot $Path
-};
+	Write-Output (path-resolve $PWD.path $Path)
+}
 
-$configs = @(
-	[pscustomobject]@{FileName='.gitconfig'; Dest=$HOME}
-	[pscustomobject]@{FileName='.gitignore_global'; Dest=$HOME}
-	[pscustomobject]@{FileName='.npmrc'; Dest=$HOME}
-	[pscustomobject]@{FileName='PowerShell_profile.ps1'; Dest=$PROFILE}
+$__configs = @(
+    [pscustomobject]@{FileName='configs\.gitconfig'; Dest=$HOME}
+    [pscustomobject]@{FileName='configs\.gitignore_global'; Dest=$HOME}
+    [pscustomobject]@{FileName='configs\.npmrc'; Dest=$HOME}
+    [pscustomobject]@{FileName='configs\PowerShell_profile.ps1'; Dest=$PROFILE}
 )
 
-For ($i=0; $i -lt $configs.Length; $i++) {
-	if($f -and (get-filename $f) -ne (get-filename $configs[$i].FileName)){
-		continue;
-	}
-	if(split-path $configs[$i].Dest -IsAbsolute){
-		$file = path-relative $configs[$i].FileName
-		$dest = $configs[$i].Dest
-		cp  $file $dest
-		echo "Copy `"$file`"`nTo `"$dest`""
-		echo "------------------------"
-	} else {
-		$file = path-relative $configs[$i].FileName
-		$dest = path-relative $configs[$i].Dest
-		cp  $file $dest
-		echo "Copy `"$file`"`nTo `"$dest`""
-		echo "------------------------"
-	}
+if($args.length -eq 1 -and $args[0] -eq '--all') {
+    $configs = $__configs
+} else {
+    $configs = @();
+    foreach($c in $__configs){
+        # skip the not found files in the $f list
+        $found = !$args
+        foreach($f in $args){
+            if($f -eq (Split-Path $c.FileName -Leaf)){
+                $found = $True;
+                break;
+            }
+        }
+        if($found) { $configs += $c }
+    }
+}
+
+foreach ($c in $configs) {
+    
+    $file = path-relative $c.FileName
+    
+    $dest = path-relative $c.Dest
+
+    $filename = file-name $file
+
+    $destfile = $dest
+    if(Test-Path $dest -PathType Container){
+        $destfile = path-resolve $dest $filename
+    }
+
+    $cachedfile = path-relative ".\.cache\$filename"
+
+    if ((Test-Path $cachedfile) -and (cat -Raw $cachedfile) -cne (cat -Raw $destfile)){
+        Write-Host "There is change in the config file:" -ForegroundColor red
+        Write-Host $destfile -ForegroundColor red
+        Write-Host "------------------------" -ForegroundColor red
+    } else {
+        if ((Test-Path $file) -and (cat -Raw $file) -cne (cat -Raw $destfile)){
+            cp $file $destfile
+            cp $file $cachedfile
+            echo ""
+            echo "Copy `"$file`""
+            echo "To `"$destfile`""
+            Write-Host "------------------------" -ForegroundColor green
+        } else {
+            echo ""
+            echo "Config `"$filename`" is not changed"
+            Write-Host "------------------------" -ForegroundColor Yellow
+        }
+    }
+
 }

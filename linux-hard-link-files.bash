875cc4ecbef570f9676c6d7d6eed6469edbfa0d7
محمد الصوالحي <ms.2052001@gmail.com>
2022-02-28 21:37:32 +0200

    refactor linking scripts to detected deleted files

--- linux-hard-link-files.bash
+++ linux-hard-link-files.bash
@@ -1,24 +1,29 @@
 #!/bin/bash
 
+PS4='$LINENO: '
+set -e
+shopt -s dotglob
+
 print_help () {
-  echo 'OPTIONS:
+cat <<EOF
+OPTIONS:
 ──┬──────────────┬───────────────────────────────────────────────────
 -h│ --help       │ help message, displays this message and ignore all options
 -l│ --list-files │ list all files without any effect
 -d│ --dry        │ no rehyal psical changes will happen
 -f│ --fill       │ only missing files will be linked (default when HOME -> ./linux)
--r│ --reverse    │ files in ./linux will be linked to your $HOME (--fill is not activated)
+-r│ --reverse    │ files in ./linux will be linked to your \$HOME (--fill is not activated)
   │ --force      │ all files will be linked forcely "ln -f"
 ──┴──────────────┴───────────────────────────────────────────────────
-'
+EOF
 }
 
 # detecting options
 is_merged_arg() {
   arg=${1:1}; shift
-  test `expr length $arg` -eq $# || return 1;
-  for a in $@; do
-    test `expr index $arg $a` -gt 0 || return 1
+  test ${#arg} -eq $# || return 1;
+  for a in "$@"; do
+    test "$(expr index "$arg" "$a")" -gt 0 || return 1
   done
   echo 1
 }
@@ -38,21 +43,22 @@ for o in "$@"; do
   [ "$o" = "--fill"       ] && is_fill=1       && continue
   [ "$o" = "--force"      ] && is_force=1      && continue
 
-  [ $(is_merged_arg $o r d)   ] && is_dry=1  && is_reverse=1 && continue
-  [ $(is_merged_arg $o r f)   ] && is_fill=1 && is_reverse=1 && continue
-  [ $(is_merged_arg $o d f)   ] && is_dry=1  && is_fill=1    && continue
-  [ $(is_merged_arg $o r d f) ] && is_dry=1  && is_fill=1    &&
+  [ "$(is_merged_arg "$o" r d)"   ] && is_dry=1  && is_reverse=1 && continue
+  [ "$(is_merged_arg "$o" r f)"   ] && is_fill=1 && is_reverse=1 && continue
+  [ "$(is_merged_arg "$o" d f)"   ] && is_dry=1  && is_fill=1    && continue
+  [ "$(is_merged_arg "$o" r d f)" ] && is_dry=1  && is_fill=1    &&
     is_reverse=1 && continue
 
-  [ $(is_merged_arg $o r h)     ] && is_help=1 && continue
-  [ $(is_merged_arg $o f h)     ] && is_help=1 && continue
-  [ $(is_merged_arg $o d h)     ] && is_help=1 && continue
-  [ $(is_merged_arg $o r d h)   ] && is_help=1 && continue
-  [ $(is_merged_arg $o r f h)   ] && is_help=1 && continue
-  [ $(is_merged_arg $o d f h)   ] && is_help=1 && continue
-  [ $(is_merged_arg $o r d f h) ] && is_help=1 && continue
+  [ "$(is_merged_arg "$o" r h)"     ] && is_help=1 && continue
+  [ "$(is_merged_arg "$o" f h)"     ] && is_help=1 && continue
+  [ "$(is_merged_arg "$o" d h)"     ] && is_help=1 && continue
+  [ "$(is_merged_arg "$o" r d h)"   ] && is_help=1 && continue
+  [ "$(is_merged_arg "$o" r f h)"   ] && is_help=1 && continue
+  [ "$(is_merged_arg "$o" d f h)"   ] && is_help=1 && continue
+  [ "$(is_merged_arg "$o" r d f h)" ] && is_help=1 && continue
 
-  echo unknown options \"$o\" >&2; exit 1
+  echo unknown options \""$o"\" >&2
+  exit 1
 
 done
 
@@ -69,10 +75,12 @@ then
   is_fill=1
 fi
 
-[ $is_reverse ] &&
-readarray -t patterns < <(find linux -type f | sed 's/^linux\///') ||
-# remove empty lines and comments
-readarray -t patterns < <(sed "/^\s*#\|^\s*$/ d" ./linux-linked-files.txt)
+if [ $is_reverse ]; then
+  readarray -t patterns < <(find linux -type f | sed 's/^linux\///')
+else
+  # remove empty lines and comments
+  readarray -t patterns < <(sed "/^\s*#\|^\s*$/ d" ./linux-linked-files.txt)
+fi
 
 # echo "${patterns[@]}"; exit
 # echo DRY=$is_dry REVERSE=$is_reverse FORCE=$is_force FILL=$is_fill; exit
@@ -87,20 +95,43 @@ files=$(
 
 (($?)) && exit 1 # exit when an error detected
 
-readarray -t files <<< "$files"
+if [ ! $is_reverse ]; then
+  # deleted_files only working whene not reversing
+  readarray -t deleted_files < <(
+    comm -23 \
+    <(find linux -type f | sed -e 's:linux/::' | sort) \
+    <(printf '%s\n' "${files[@]}" | sort)
+  )
+fi
+
+if [ $is_fill ]; then
+  readarray -t files < <(
+    comm -13 \
+    <(find linux -type f | sed -e 's:linux/::' | sort) \
+    <(printf '%s\n' "${files[@]}" | sort)
+  )
+else
+  readarray -t files <<< "${files[@]}"
+fi
 
 if [ $is_list_files ]; then
   printf "%s\n" "${files[@]}"
   exit
 fi
 
-force_opt=`test $is_force && echo '-f'`
 
-for f in "${files[@]}"; do
-  echo "$f"
+printf "%s\n" "${deleted_files[@]/#/- }"
+[ ! $is_dry ] &&
+((${#deleted_files[@]})) &&
+rm "${deleted_files[@]/#/linux\/}"
+
+force_opt=$(test $is_force && echo '-f')
+while read -r f; do
+  echo "+ $f"
   [ $is_dry ] && continue
-  [ ! $is_reverse ] && 
-    ln $force_opt "$HOME/$f" "./linux/$f" ||
-    ln $force_opt "./linux/$f" "$HOME/$f"
-done
-
+  if [ ! $is_reverse ]; then
+    ln "$force_opt" "$HOME/$f" "./linux/$f"
+  else
+    ln "$force_opt" "./linux/$f" "$HOME/$f"
+  fi
+done <<< "${files[@]}"
--- linux-linked-files.js
+++ linux-linked-files.js
@@ -12,13 +12,44 @@
 const fs = require("fs");
 const path = require("path");
 const patterns = process.argv.slice(2);
-const relPaths = new Set();
+const relativePaths = new Set();
 const linkDirs = new Set();
 const isDry = !!process.env.DRY;
 const isForce = !!process.env.FORCE;
 const isReverse = !!process.env.REVERSE;
 const isFill = !!process.env.FILL;
-const errorOccured = false;
+let errorOccured = false;
+
+while (patterns.length) {
+  const p = patterns.shift();
+  if (!isIncludedFile(p)) continue;
+  const linkingData = getFileData(p);
+
+  if (!fs.existsSync(linkingData.path))
+    error("source path doesn't exist: " + linkingData.path);
+
+  // if it is file
+  if (fs.statSync(linkingData.path).isFile()) {
+    if (fs.existsSync(linkingData.link)) {
+      // this is now handled in the bash script
+      // if (isFill) continue; // skip this file as it exists
+      if (!isForce && !isFill)
+        error(
+          "targetted link will overwrite exsiting file: " + linkingData.link
+        );
+    }
+    relativePaths.add(linkingData.relPath);
+    linkDirs.add(linkingData.linkDir);
+  } else {
+    // it is directory
+    let content = fs
+      .readdirSync(linkingData.path, { withFileTypes: true })
+      .map((dirent) => resolve(linkingData.path, dirent.name))
+      .filter(isIncludedFile);
+    // put the at the beginning of our patterns, FIFO
+    patterns.unshift(...content);
+  }
+}
 
 function resolve(...p) {
   let homeRootPath = p.find((_) => _.startsWith("~"));
@@ -27,35 +58,14 @@ function resolve(...p) {
   return path.resolve(...p);
 }
 
-function isIgnored(p) {
-  return (
-    patterns.findIndex(function (ignorePattern) {
-      if (ignorePattern[0] !== "!") return false; // it is a pattern to include files
-      ignorePattern = resolve(ignorePattern.slice(1)); // get rid of "!"
-      p = resolve(p);
-      if (ignorePattern === p) return true; // exact match
-      // ignore sub files and directories if the ignore pattern is a directory
-      if (!fs.existsSync(ignorePattern)) return false;
-      if (
-        fs.statSync(ignorePattern).isDirectory() &&
-        !path.relative(ignorePattern, p).startsWith("..")
-      )
-        return true;
-    }) > -1
-  );
-}
-
-function filter(p) {
-  return !p.startsWith("!") && !isIgnored(p);
-}
-
-function handleFile(f) {
+function getFileData(f) {
   let relPath = f;
   if (!isReverse) {
     relPath = resolve(f); // if starts with ~, resolve it to get the full path
     relPath = path.relative(process.env.HOME, relPath);
-    if (relPath.startsWith(".."))
-      throw "patterns should be inside the $HOME directory";
+    if (relPath.startsWith("..")) {
+      error("patterns should be inside the $HOME directory");
+    }
   }
   let linksRootDir = isReverse ? process.env.HOME : "./linux";
   let srcRootDir = isReverse ? "./linux" : process.env.HOME;
@@ -68,43 +78,34 @@ function handleFile(f) {
   };
 }
 
-while (patterns.length) {
-  let p = patterns.shift();
-  if (filter(p)) {
-    let linkingData = handleFile(p);
-    if (!fs.existsSync(linkingData.path)) {
-      console.error("source path doesn't exist: " + linkingData.path);
-      errorOccured = true;
-    }
+function isIncludedFile(p) {
+  return !p.startsWith("!") && !isIgnored(p);
+}
 
-    // if it is file
-    if (fs.statSync(linkingData.path).isFile()) {
-      if (fs.existsSync(linkingData.link)) {
-        if (isFill) continue; // skip this file as it exists
-        if (!isForce) {
-          console.error(
-            "targetted link will overwrite exsiting file: " + linkingData.link
-          );
-          errorOccured = true;
-        }
-      }
-      relPaths.add(linkingData.relPath);
-      linkDirs.add(linkingData.linkDir);
-    }
+function isIgnored(pathToCheck) {
+  return (
+    patterns.findIndex(function (ignorePattern) {
+      if (ignorePattern[0] !== "!") return false; // it is a pattern to include files
+      ignorePattern = resolve(ignorePattern.slice(1)); // get rid of "!"
+      pathToCheck = resolve(pathToCheck);
+      if (ignorePattern === pathToCheck) return true; // exact match
+      // ignore sub files and directories if the ignore pattern is a directory
+      if (!fs.existsSync(ignorePattern)) return false;
+      if (
+        fs.statSync(ignorePattern).isDirectory() &&
+        !path.relative(ignorePattern, pathToCheck).startsWith("..")
+      )
+        return true;
+    }) > -1
+  );
+}
 
-    // it is directory
-    else {
-      let content = fs
-        .readdirSync(linkingData.path, { withFileTypes: true })
-        .map((dirent) => resolve(linkingData.path, dirent.name))
-        .filter(filter);
-      patterns.unshift(...content); // put the at the beginning of our patterns, FIFO
-    }
-  }
+function error(...msgs) {
+  console.error(...msgs);
+  errorOccured = true;
 }
 
 if (errorOccured) process.exit(1);
 
 !isDry && linkDirs.forEach((d) => fs.mkdirSync(d, { recursive: true }));
-relPaths.forEach((f) => console.log(f));
-// console.log(Array.from(relPaths).join("\n"));
+relativePaths.forEach((f) => console.log(f));
